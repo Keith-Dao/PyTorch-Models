d7d42b23ea5134a7e6dbcfa3ba76fb459a11769c
Keith Dao <keith.dao.248@gmail.com>
2023-08-01 00:12:35 +1000

    Adds modern LeNet-5.

---
+++ CNN/LeNet/modern/lenet.py
@@ -0,0 +1,35 @@
+"""
+    Modern interpretation of the LeNet-5 model.
+"""
+import torch
+from torch import nn
+
+
+class LeNet5(nn.Module):
+    """
+    Modern interpretation of the LeNet-5 model.
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.activation = nn.ReLU()
+        self.c1 = nn.Conv2d(1, 6, (5, 5))
+        self.s2 = nn.MaxPool2d((2, 2), (2, 2))
+        self.c3 = nn.Conv2d(6, 16, (5, 5))
+        self.s4 = nn.MaxPool2d((2, 2), (2, 2))
+        self.c5 = nn.Conv2d(16, 120, (5, 5))
+        self.f6 = nn.Linear(120, 84)
+        self.out = nn.Linear(84, 10)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass.
+        """
+        x = self.activation(self.c1(x))
+        x = self.activation(self.s2(x))
+        x = self.activation(self.c3(x))
+        x = self.activation(self.s4(x))
+        x = self.activation(self.c5(x))
+        x = torch.flatten(x, start_dim=1)
+        x = self.f6(x)
+        return self.out(x)

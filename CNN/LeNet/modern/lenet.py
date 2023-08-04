ad04a0160e11dca9560e3681f8a525666b38590e
Keith Dao <keith.dao.248@gmail.com>
2023-08-04 19:36:05 +1000

    Adds modern LeNet-5 feature map extraction.

--- CNN/LeNet/modern/lenet.py
+++ CNN/LeNet/modern/lenet.py
@@ -21,14 +21,25 @@ class LeNet5(nn.Module):
         self.f6 = nn.Linear(120, 84)
         self.out = nn.Linear(84, 10)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(
+        self, x: torch.Tensor, feature_maps: None | list[torch.Tensor] = None
+    ) -> torch.Tensor:
         """
         Forward pass.
         """
+        if feature_maps is not None:
+            feature_maps.clear()
+
         x = self.activation(self.c1(x))
         x = self.activation(self.s2(x))
+        if feature_maps is not None:
+            feature_maps.append(x)
+
         x = self.activation(self.c3(x))
         x = self.activation(self.s4(x))
+        if feature_maps is not None:
+            feature_maps.append(x)
+
         x = self.activation(self.c5(x))
         x = torch.flatten(x, start_dim=1)
         x = self.f6(x)

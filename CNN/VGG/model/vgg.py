3433b6492072eefc0bd9edc6e3ab61bd8f19e147
Keith Dao <keith.dao.248@gmail.com>
2023-09-29 17:42:25 +1000

    Changes ReLU to be done inplace.

--- CNN/VGG/model/vgg.py
+++ CNN/VGG/model/vgg.py
@@ -14,41 +14,41 @@ class VGG(nn.Module):
         super().__init__()
         self.net = nn.Sequential(  # (3, 224, 224)
             nn.Conv2d(3, 64, 3, padding=1),  # (64, 224, 224)
-            nn.ReLU(),
+            nn.ReLU(True),
             nn.Conv2d(64, 64, 3, padding=1),  # (64, 224, 224)
-            nn.ReLU(),
+            nn.ReLU(True),
             nn.MaxPool2d(2, 2),  # (64, 112, 112)
             nn.Conv2d(64, 128, 3, padding=1),  # (128, 112, 112)
-            nn.ReLU(),
+            nn.ReLU(True),
             nn.Conv2d(128, 128, 3, padding=1),  # (128, 112, 112)
-            nn.ReLU(),
+            nn.ReLU(True),
             nn.MaxPool2d(2, 2),  # (128, 56, 56)
             nn.Conv2d(128, 256, 3, padding=1),  # (256, 56, 56)
-            nn.ReLU(),
+            nn.ReLU(True),
             nn.Conv2d(256, 256, 3, padding=1),  # (256, 56, 56)
-            nn.ReLU(),
+            nn.ReLU(True),
             nn.Conv2d(256, 256, 3, padding=1),  # (256, 56, 56)
-            nn.ReLU(),
+            nn.ReLU(True),
             nn.MaxPool2d(2, 2),  # (256, 28, 28)
             nn.Conv2d(256, 512, 3, padding=1),  # (512, 28, 28)
-            nn.ReLU(),
+            nn.ReLU(True),
             nn.Conv2d(512, 512, 3, padding=1),  # (512, 28, 28)
-            nn.ReLU(),
+            nn.ReLU(True),
             nn.Conv2d(512, 512, 3, padding=1),  # (512, 28, 28)
-            nn.ReLU(),
+            nn.ReLU(True),
             nn.MaxPool2d(2, 2),  # (512, 14, 14)
             nn.Conv2d(512, 512, 3, padding=1),  # (512, 14, 14)
-            nn.ReLU(),
+            nn.ReLU(True),
             nn.Conv2d(512, 512, 3, padding=1),  # (512, 14, 14)
-            nn.ReLU(),
+            nn.ReLU(True),
             nn.Conv2d(512, 512, 3, padding=1),  # (512, 14, 14),
-            nn.ReLU(),
+            nn.ReLU(True),
             nn.MaxPool2d(2, 2),  # (512, 7, 7)
             nn.Flatten(),
             nn.Linear(25088, 4096),
-            nn.ReLU(),
+            nn.ReLU(True),
             nn.Linear(4096, 4096),
-            nn.ReLU(),
+            nn.ReLU(True),
             nn.Linear(4096, num_classes),
         )
 

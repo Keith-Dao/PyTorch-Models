4d377bc2c3fc9aad63fe4ec26cf1c5f27930cf2a
Keith Dao <keith.dao.248@gmail.com>
2025-04-18 16:25:48 +1000

    Move files into main package folder.

---
+++ pytorch_models/CNN/inception_v3/__init__.py
@@ -0,0 +1,4 @@
+from .model.inception_v3 import InceptionV3
+from .regularisation.label_smoothing import LabelSmoothing
+
+__all__ = ["InceptionV3", "LabelSmoothing"]
---
+++ pytorch_models/CNN/lenet/__init__.py
@@ -0,0 +1,4 @@
+from modern.lenet import LeNet5
+from original.lenet import LeNet5 as OriginalLeNet5
+
+__all__ = ["LeNet5", "OriginalLeNet5"]
